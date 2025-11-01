3b3dad77045b94a5e3f9a3e9f460c1aef9e04e6b
Douglas Baggett <berths-purger-0g@icloud.com>
2025-11-01 11:49:50 -0400

    Added extremetuxracer

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux samba samba-client samba-common-tools
+dnf5 install -y tmux extremetuxracer 
 
 # Use a COPR Example:
 #
